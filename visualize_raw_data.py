6f46bdfaa23909ea7a6c4925e558d38dc338b06d
James Jeon <jkh911208@naver.com>
2017-05-10 11:01:51 -0500

    visualize added

---
+++ visualize_raw_data.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+
+raw_data = np.load("1494431577.559893.npy")
+print(raw_data[0][0].shape)
+
+for data in raw_data:
+    front_frame = data[0]
+    front_frame = cv2.flip(front_frame,0)
+    front_frame = cv2.flip(front_frame,1)
+
+    cv2.imshow('front_frame',front_frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#front_frame.release()
+cv2.destroyAllWindows()
